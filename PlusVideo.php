a98810b69c5e95e8f7ce65683fad2917f905217d
Jesus B. Nana <kurapikats@yahoo.com>
2013-11-14 15:04:09 +0800

    Add config read, merge and process js file

--- PlusVideo.php
+++ PlusVideo.php
@@ -1,32 +1,202 @@
 <?php
+/**
+ * PlusVideo: Combine config and upload to S3 and Cloudfront
+ *
+ * @category  Advertising
+ * @package   VideoOtp
+ * @author    Jesus B. Nana <jesus.nana@adtouch.com>
+ * @copyright 2013 VideoOTP
+ * @license   [url] [description]
+ * @version   0.1
+ * @link      http://videootp.com
+ */
 namespace VideoOtp\PlusVideo;
 
 use VideoOtp\S3Uploader;
 
 require_once 'S3Uploader.php';
 
+/**
+ * S3 Uploader
+ *
+ * @category S3
+ * @package  Amazon
+ * @author   Jesus B. Nana <jesus.nana@adtouch.com>
+ * @license  [url] [description]
+ * @version  0.1
+ * @link     http://videootp.com
+ */
 class PlusVideo
 {
-    public function __constructor()
-    {
+    private $_conf;
+    private $_accessKey;
+    private $_secretKey;
 
+    /**
+     * [__construct description]
+     */
+    public function __construct()
+    {
+        $this->_conf = $this->loadConfig(__DIR__ . '/' . 'access.conf');
+        $this->_accessKey = $this->_conf->AWS->accessKey;
+        $this->_secretKey = $this->_conf->AWS->secretKey;
     }
 
+    /**
+     * [loadConfig description]
+     *
+     * @param string $file [comment]
+     *
+     * @return [type]
+     */
+    public function loadConfig($file)
+    {
+        $source = $file;
+
+        if (file_exists($source)) {
+            $content = file_get_contents($source);
+            $data = json_decode($content);
+
+            return $data;
+        } else {
+            echo $source;
+        }
+
+        return false;
+    }
+
+    /**
+     * Pass the js file to process
+     *
+     * @param string $jsFile relative path of js file
+     *
+     * @return mixed
+     */
     public function processJs($jsFile)
     {
-        echo $jsFile;
+        $this->_createBackup($jsFile);
 
-        $access = array(
-            'key'    => 'XXX',
-            'secret' => 'XXX',
+        $jsFileContent = file_get_contents($jsFile);
+        // match all the configuration variable and put inside $contentVars
+        $contentVars = array();
+        $userVarPattern = '/^.*\bVOTP(.*)\b.*$/m';
+        preg_match_all($userVarPattern, $jsFileContent, $contentVars);
+
+        $replacements = $this->_mergeDefaultValues();
+
+        $patterns = array();
+        if (isset($contentVars) && !empty($contentVars)) {
+            foreach ($contentVars[1] as $v) {
+                $votpConfVar = 'VOTP' . $v;
+                $patterns[$votpConfVar] = '/{' . $votpConfVar . '}/';
+            }
+        }
+
+        // always ksort both arrays when doing preg_replace
+        ksort($patterns);
+        ksort($replacements);
+        $updatedJs = preg_replace($patterns, $replacements, $jsFileContent);
+
+        file_put_contents($jsFile, $updatedJs);
+    }
+
+    /**
+     * Alias of loadConfig(), get the user's configurations
+     *
+     * @param string $defaultConfFile path to config file
+     * @param string $userConfFile    path to config file
+     *
+     * @return [type]
+     */
+    private function _mergeDefaultValues(
+        $defaultConfFile = 'defaults.conf',
+        $userConfFile = 'settings.conf'
+    ) {
+        $defaultValues = (array) $this->parseUserValues($defaultConfFile);
+        $userValues = (array) $this->parseUserValues($userConfFile);
+
+        $mergedValues = array_merge($defaultValues, $userValues);
+
+        return $mergedValues;
+    }
+
+
+    /**
+     * Alias of loadConfig(), get the user's configurations
+     *
+     * @param string $configFile path to config file
+     *
+     * @return [type]
+     */
+    public function parseUserValues($configFile = 'settings.conf')
+    {
+        return $this->loadConfig($configFile);
+    }
+
+    /**
+     * [getVastXmlFromJs description]
+     *
+     * @param string $path2D30Js ie. dir/d30_player.js
+     *
+     * @return mixed minified vastXML on success
+     *                false on failure
+     */
+    public function getVastXmlFromJs($path2D30Js)
+    {
+        if (file_exists($path2D30Js)) {
+            $source = file_get_contents($path2D30Js);
+        } else {
+            return false;
+        }
+
+        $matches = array();
+        preg_match(
+            '/shortTail_D30.vastXML = \'\<\?xml(.*)/', $source, $matches
         );
-        $x = new S3Uploader($access, 'video-d30');
-        $upload = $x->uploadDirectory(__DIR__);
 
-        // prepend 'a-' for easy browsing on aws console
-        var_dump($upload);
-        exit;
+        if (isset($matches[1]) AND !empty($matches[1])) {
+            // append the opening xml tag
+            $opXml = '<?xml';
+            $vastXml = $opXml . $matches[1];
 
+            return $vastXml;
+        }
+
+        return false;
+    }
+
+    /**
+     * Creates a backup of the target file
+     *
+     * @param string $file [description]
+     *
+     * @return $mixed filename of the backup file on success
+     *                false on failure
+     */
+    private function _createBackup($file)
+    {
+        if (file_exists($file)) {
+            // append timestamp on backup filename
+            $pathInfo = pathinfo($file);
+            $backupFile = $pathInfo['dirname'] . '/' . $pathInfo['filename'] .
+                '.' . date('m-d-Y-h-i-s') . '.' . $pathInfo['extension'] .
+                '.bak';
+
+            if (!copy($file, $backupFile)) {
+                throw new \Exception(
+                    sprintf('Unable to backup "%s" ', $file)
+                );
+            }
+
+            return $backupFile;
+
+        } else {
+            throw new \Exception(
+                sprintf('File "%s" doesn\'t exist', $file)
+            );
+        }
+
+        return false;
     }
 }
 
--- S3Uploader.php
+++ S3Uploader.php
@@ -1,45 +1,71 @@
 <?php
+/**
+ * Amazon S3 File and Directory Uploader for VideoOTP
+ *
+ * @category  Cloud_Computing
+ * @package   VideoOtp_Amazon
+ * @author    Jesus B. Nana <jesus.nana@adtouch.com>
+ * @copyright 2013 VideoOTP
+ * @license   [url] [description]
+ * @version   0.1
+ * @link      http://videootp.com
+ */
 namespace VideoOtp\S3Uploader;
 
 use Aws\S3\S3Client;
 
 require_once 'libraries' . DIRECTORY_SEPARATOR . 'aws.phar';
 
-class S3Uploader {
+/**
+ * S3 Uploader
+ *
+ * @category S3
+ * @package  Amazon
+ * @author   Jesus B. Nana <jesus.nana@adtouch.com>
+ * @license  [url] [description]
+ * @version  0.1
+ * @link     http://videootp.com
+ */
+class S3Uploader
+{
 
-    private $client;
-    private $bucket;
+    private $_client;
+    private $_bucket;
 
     /**
      * Pass the amazon access key/secret
      * $access['key']
      * $access['secret']
+     *
      * @param array  $access Amazon access credentials
      * @param string $bucket S3 bucket name
      */
     public function __construct(array $access = array(), $bucket = 'video-d30')
     {
         // TODO: add checking here
-        $this->client = S3Client::factory(array(
-            'key' => $access['key'],
-            'secret' => $access['secret'],
-        ));
+        $this->_client = S3Client::factory(
+            array(
+                'key' => $access['key'],
+                'secret' => $access['secret']
+            )
+        );
 
-        $this->bucket = $bucket;
+        $this->_bucket = $bucket;
 
-        $this->client->registerStreamWrapper();
+        $this->_client->registerStreamWrapper();
     }
 
     /**
      * Upload a file on S3, if the file exist backup the old file
      * and append timestamp on the filename
      *
-     * @param  string $filename        local file to be uploaded,
-     *                                 relative or absolute file
-     * @param  string $remoteDirectory Remote directory where to upload the file
-     * @param  string $prefix          Defaults to "a-", this is to make
-     *                                 browsing on S3 console easier
-     * @return string url              Returns the generated S3 url
+     * @param string $filename        local file to be uploaded,
+     *                                relative or absolute file
+     * @param string $remoteDirectory Remote directory where to upload the file
+     * @param string $prefix          Defaults to "a-", this is to make
+     *                                browsing on S3 console easier
+     *
+     * @return mixed url generated S3 url or false on failure
      */
     public function uploadFile($filename, $remoteDirectory = "", $prefix = "a-")
     {
@@ -56,7 +82,7 @@ class S3Uploader {
             }
 
             try {
-                $s3Url = 's3://' . $this->bucket . '/';
+                $s3Url = 's3://' . $this->_bucket . '/';
 
                 // if it already exist on the bucket rename it
                 if (file_exists($s3Url . $key)) {
@@ -65,11 +91,13 @@ class S3Uploader {
                 }
 
                 // Upload an object to Amazon S3
-                $result = $this->client->putObject(array(
-                    'Bucket' => $this->bucket,
-                    'Key' => $key,
-                    'SourceFile' => $filename,
-                ));
+                $result = $this->_client->putObject(
+                    array(
+                        'Bucket' => $this->_bucket,
+                        'Key' => $key,
+                        'SourceFile' => $filename,
+                    )
+                );
 
                 return $result['ObjectURL'];
 
@@ -77,8 +105,9 @@ class S3Uploader {
                 throw new \Exception($e->getMessage());
             }
         } else {
-            throw new \Exception(sprintf('File "%s" doesn\'t exist',
-                $filename));
+            throw new \Exception(
+                sprintf('File "%s" doesn\'t exist', $filename)
+            );
         }
 
         return false;
@@ -88,9 +117,13 @@ class S3Uploader {
      * Upload a directory to S3
      * ie. uploading D:\projects\sandbox\video-otp
      * will use 'video-otp' as it's remote directory
-     * @param  string $localDirectory Target directory to upload
-     * @param  string $prefix         Defaults to "a-", this is to make
-     *                                browsing on S3 console easier
+     *
+     * @param string $localDirectory Target directory to upload
+     * @param string $prefix         Defaults to "a-", this is to make
+     *                               browsing on S3 console easier
+     *
+     * @return mixed Array of uploaded files on success
+     *               false on failure
      */
     public function uploadDirectory($localDirectory, $prefix = "a-")
     {
@@ -99,22 +132,31 @@ class S3Uploader {
             // use the current directory as remote directory
             $path = explode(DIRECTORY_SEPARATOR, $localDirectory);
             $remoteDirectory = end($path);
+            $uploaded = array();
 
             // TODO: improve this to handle recursive iteration
             $dir = new \FilesystemIterator($localDirectory);
             foreach ($dir as $fileinfo) {
                 if ($fileinfo->isFile()) {
-                    // upload the files
-                    $upload = $this->uploadFile($fileinfo->getFilename(),
-                        $prefix . $remoteDirectory);
 
-                    echo $upload . "\n";
+                    // upload the files
+                    $upload = $this->uploadFile(
+                        $fileinfo->getFilename(),
+                        $prefix . $remoteDirectory
+                    );
+
+                    $uploaded[] = $upload;
                 }
             }
 
+            return $uploaded;
+
         } else {
-            throw new \Exception(sprintf('Directory "%s" doesn\'t exist',
-                $localDirectory));
+            throw new \Exception(
+                sprintf('Directory "%s" doesn\'t exist', $localDirectory)
+            );
         }
+
+        return false;
     }
 }
